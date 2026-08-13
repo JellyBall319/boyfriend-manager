f190ff2ff2e57dfa2bf08eda101e3ea71bcc14c3
JellyBall319 <73572136+JellyBall319@users.noreply.github.com>
2026-08-13 14:56:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 window.ENV_CONFIG = {
-  GITHUB_TOKEN: "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" // 👈 在這裡貼上你的 Token
+  GITHUB_TOKEN: "" // 👈 在這裡貼上你的 Token
 };
